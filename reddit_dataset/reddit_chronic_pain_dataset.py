331ec2fbd69ec523429ba77a72fe82d50b6065fd
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2022-05-01 13:39:06 -0400

    commit

--- reddit_dataset/reddit_chronic_pain_dataset.py
+++ reddit_dataset/reddit_chronic_pain_dataset.py
@@ -15,7 +15,7 @@ def main():
     reddit_dir = "/var/reddit/"
     output_folder = "chronic_pain_dataset"
     input_file = sys.argv[1]
-    files = sc.textFile(reddit_dir + input_file)
+    files = sc.textFile(reddit_dir + input_file + "*")
 
     text = files.map(json.loads).filter(lambda t: t["subreddit"] == "ChronicPain" and "selftext" in t).map(
         json.dumps).repartition(1).saveAsTextFile(output + output_folder)
