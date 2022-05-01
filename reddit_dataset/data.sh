bbce3070a1fcc8e3324723d6c9c8d78f4218d48b
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2022-05-01 13:10:39 -0400

    Update data.sh

--- reddit_dataset/data.sh
+++ reddit_dataset/data.sh
@@ -1,3 +1,6 @@
+export PYSPARK_PYTHON="/bin/python3"
+export PYSPARK_DRIVER_PYTHON="/bin/python3"
+
 spark-submit --master yarn --queue umsi-qmei \
 --num-executors 40 --executor-cores 5 --executor-memory 10G \
 reddit_chronic_pain_dataset.py RS_2019-12
