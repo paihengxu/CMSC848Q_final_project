8467629893f9ca4d06759333372f047750035a41
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2022-05-01 12:21:49 -0400

    commit

---
+++ reddit_dataset/reddit_chronic_pain_dataset.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import json
+import sys
+
+from pyspark import SparkConf, SparkContext
+
+sc_name = "Reddit Dataset"
+sc = SparkContext(
+    conf=SparkConf().set("spark.files.ignoreCorruptFiles", "true").set("spark.default.parallelism", 100).setAppName(
+        sc_name))
+
+
+def main():
+    output = "/user/tonyzhou/reddit/"
+    reddit_dir = "/var/reddit/"
+    output_folder = "chronic_pain_dataset"
+    input_file = sys.argv[0]
+    files = sc.textFile(reddit_dir + input_file)
+
+    text = files.map(json.loads).filter(lambda t: t["subreddit"] == "ChronicPain").map(json.dumps).saveAsTextFile(
+        output + output_folder)
+    sc.stop()
+
+
+if __name__ == '__main__':
+    main()
