256e306eec3f086af6dbe345085aadc265f9beeb
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2022-05-01 13:54:53 -0400

    Update data.sh

--- reddit_dataset/data.sh
+++ reddit_dataset/data.sh
@@ -3,4 +3,4 @@ export PYSPARK_DRIVER_PYTHON=python2.7
 rm -rf /hadoop-fuse/user/tonyzhou/reddit/chronic_pain_dataset/
 spark-submit --master yarn --queue umsi-qmei \
 --num-executors 40 --executor-cores 5 --executor-memory 10G \
-reddit_chronic_pain_dataset.py RS_2019
+reddit_chronic_pain_dataset.py RS_201
