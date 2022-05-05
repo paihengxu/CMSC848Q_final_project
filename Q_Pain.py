9924ce59a0e2f8967aeb8e8e9b101c9816d261b9
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2022-05-04 23:04:57 -0400

    Update Q_Pain.py

--- Q_Pain.py
+++ Q_Pain.py
@@ -44,8 +44,8 @@ repeat_number = 10
 
 medical_context_file = opts.medical_context_file
 vignettes = pd.read_csv(medical_context_file)
-data = vignettes["Yes" in vignettes.Answer]
-closed = vignettes["No" in vignettes.Answer]
+data = vignettes[vignettes.Answer == "Yes." or vignettes.Answer == "Yes"]
+closed = vignettes[vignettes.Answer == "No." or vignettes.Answer == "No"]
 
 # print(closed)
 # print(vignettes)
