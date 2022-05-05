0e128570fb49aec596528576fdeb1a5e5d27a4ca
ZYH <31127200+Tonyzhou98@users.noreply.github.com>
2022-05-04 22:44:00 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 srun --pty --qos=gpu-medium --time=8:00:00 --partition=gpu --gres=gpu:gtxtitanx:1 --exclude=materialgpu00 \
 --mem=64g /fs/clip-emoji/tonyzhou/anaconda3/envs/q_pain/bin/python Q_Pain.py --medical_context_file "data_acute_cancer.csv"
+srun --pty --qos=gpu-medium --time=8:00:00 --partition=gpu --gres=gpu:gtxtitanx:1 --exclude=materialgpu00 \
+--mem=64g /fs/clip-emoji/tonyzhou/anaconda3/envs/q_pain/bin/python Q_Pain.py --medical_context_file "data_acute_non_cancer.csv"
+srun --pty --qos=gpu-medium --time=8:00:00 --partition=gpu --gres=gpu:gtxtitanx:1 --exclude=materialgpu00 \
+--mem=64g /fs/clip-emoji/tonyzhou/anaconda3/envs/q_pain/bin/python Q_Pain.py --medical_context_file "data_chronic_cancer.csv"
+srun --pty --qos=gpu-medium --time=8:00:00 --partition=gpu --gres=gpu:gtxtitanx:1 --exclude=materialgpu00 \
+--mem=64g /fs/clip-emoji/tonyzhou/anaconda3/envs/q_pain/bin/python Q_Pain.py --medical_context_file "data_chronic_non_cancer.csv"
+srun --pty --qos=gpu-medium --time=8:00:00 --partition=gpu --gres=gpu:gtxtitanx:1 --exclude=materialgpu00 \
+--mem=64g /fs/clip-emoji/tonyzhou/anaconda3/envs/q_pain/bin/python Q_Pain.py --medical_context_file "data_post_op.csv"
