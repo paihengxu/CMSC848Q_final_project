4fb84a8b91b483716a96b2fa0142f36280105a20
paihengxu <xupaiheng@hotmail.com>
2022-05-06 22:20:56 -0400

    iterate all yes prompts as closed prompts

--- Q_Pain.py
+++ Q_Pain.py
@@ -29,18 +29,10 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # print(device)
 
 model.cuda(device)
-# print(all(p.is_cuda for p in model.parameters()))
-
-# Shuffle the names for each of the ten vignettes
-for g in genders:
-    for r in races:
-        random.shuffle(names[r][g])
-
 # Prompting GPT-3 for each prompt x gender x race combo
-# Saving the results as a json
 
 out1 = []
-repeat_number = 10
+repeat_number = len(data)  # how many open prompts per setting, i.e., number of yes prompts and names per combo
 
 medical_context_file = opts.medical_context_file
 
@@ -55,82 +47,96 @@ closed = vignettes[vignettes.Answer == "No."]
 # print(closed)
 # print(vignettes)
 
-for q in range(10):
-    open_prompt_standard = create_open_standard(q)
-    print("---------- DOING VIGNETTE #", q, " ----------")
+# iterate through all yes examples
+for i in range(len(data)):
 
-    # Sampling closed prompts
-    available = [i for i in range(10) if i != q]
-    sample = random.sample(available, 2)
-    # print(closed)
+    # Shuffle the names for each of the ten vignettes
+    for g in genders:
+        for r in races:
+            random.shuffle(names[r][g])
 
-    closed_promptNo = "Vignette: " + list(closed.Vignette)[0] + "\n" + \
-                      "Question: " + list(closed.Question)[0] + "\n" + \
-                      "Answer: " + list(closed.Answer)[0] + "\n" + \
-                      "Dosage: " + str(closed.Dosage)[0] + "\n" + \
-                      "Explanation: " + list(closed.Explanation)[0] + "##\n\n"
+    for q in range(repeat_number):
+        # we don't want yes closed prompt to be the same as open prompt
+        if i == q:
+            continue
 
-    closed_prompt0 = create_closed(sample[0], dose_high)
-    closed_prompt2 = create_closed(sample[1], dose_low)
+        open_prompt_standard = create_open_standard(q)
+        print("---------- DOING VIGNETTE #", q, " ----------")
 
-    # Generate by going through genders and races
-    for closed_gender in genders:
-        for closed_race in races:
+        # Sampling closed prompts
+        # available = [i for i in range(10) if i != q]
+        # sample = random.sample(available, 2)
+        # print(closed)
 
-            if if_biased:
-                closed_promptNo = race_gender_closed(closed_promptNo, 'Patient C', closed_gender, closed_race)
-                closed_prompt0 = race_gender_closed(closed_prompt0, 'Patient A', closed_gender, closed_race)
-                closed_prompt2 = race_gender_closed(closed_prompt0, 'Patient B', closed_gender, closed_race)
+        closed_promptNo = "Vignette: " + list(closed.Vignette)[0] + "\n" + \
+                          "Question: " + list(closed.Question)[0] + "\n" + \
+                          "Answer: " + list(closed.Answer)[0] + "\n" + \
+                          "Dosage: " + str(closed.Dosage)[0] + "\n" + \
+                          "Explanation: " + list(closed.Explanation)[0] + "##\n\n"
 
-            else:
-                closed_promptNo = standardize_closed(closed_promptNo, 'Patient C')
-                closed_prompt0 = standardize_closed(closed_prompt0, 'Patient A')
-                closed_prompt2 = standardize_closed(closed_prompt2, 'Patient B')
+        # closed_prompt0 = create_closed(sample[0], dose_high)
+        closed_prompt0 = create_closed(i, dose_high)
+        # closed_prompt2 = create_closed(sample[1], dose_low)
 
-                closed_gender = "None"
-                closed_race = "None"
+        # Generate by going through genders and races
+        for closed_gender in genders:
+            for closed_race in races:
 
-            closed_prompt = closed_prompt0 + closed_promptNo  # +closed_prompt2
+                if if_biased:
+                    closed_promptNo = race_gender_closed(closed_promptNo, 'Patient C', closed_gender, closed_race)
+                    closed_prompt0 = race_gender_closed(closed_prompt0, 'Patient A', closed_gender, closed_race)
+                    # closed_prompt2 = race_gender_closed(closed_prompt0, 'Patient B', closed_gender, closed_race)
 
-            for g in genders:
-                open_prompt_gendered = genderize_open(open_prompt_standard, g)
-                for r in races:
-                    response = {}  # initialize response for GPT-2
-                    open_prompt = race_name_open(open_prompt_gendered, r, q, g)
-                    final_prompt = closed_prompt + open_prompt
+                else:
+                    closed_promptNo = standardize_closed(closed_promptNo, 'Patient C')
+                    closed_prompt0 = standardize_closed(closed_prompt0, 'Patient A')
+                    # closed_prompt2 = standardize_closed(closed_prompt2, 'Patient B')
 
-                    indexed_tokens = tokenizer.encode(final_prompt)
-                    tokens_tensor = torch.tensor([indexed_tokens]).to(device)
+                    closed_gender = "None"
+                    closed_race = "None"
 
-                    with torch.no_grad():
-                        predictions = model(tokens_tensor)
-                        results = predictions[0]
-                        temp = results[0, -1, :]
-                        temp = temp.cpu().numpy()
-                        result = softmax(temp)
-                        word_1 = tokenizer.encode(' Yes')[0]
-                        word_2 = tokenizer.encode(' No')[0]
+                closed_prompt = closed_prompt0 + closed_promptNo  # +closed_prompt2
 
-                    pred_id = torch.argmax(results[0, -1, :]).item()
-                    # print("\nPredicted token ID of next word: ")
-                    # print(pred_id)
+                for g in genders:
+                    open_prompt_gendered = genderize_open(open_prompt_standard, g)
+                    for r in races:
+                        response = {}  # initialize response for GPT-2
+                        open_prompt = race_name_open(open_prompt_gendered, r, q, g)
+                        final_prompt = closed_prompt + open_prompt
 
-                    pred_word = tokenizer.decode(pred_id)
-                    # print("\nPredicted next word for sequence: ")
-                    # print(pred_word)
+                        indexed_tokens = tokenizer.encode(final_prompt)
+                        tokens_tensor = torch.tensor([indexed_tokens]).to(device)
 
-                    response['context'] = medical_context_file.split('.')[0]
-                    response['yes_prob'] = result[word_1]
-                    response['no_prob'] = result[word_2]
-                    response['next_word_pred'] = pred_word
-                    response['closed_prompt_race'] = closed_race
-                    response['closed_prompt_gender'] = closed_gender
-                    response['open_prompt_race'] = r
-                    response['open_prompt_gender'] = g
-                    response['open_prompt_name'] = names[r][g][q]
-                    response['vignette_num'] = q
-                    out1.append(response)
+                        with torch.no_grad():
+                            predictions = model(tokens_tensor)
+                            results = predictions[0]
+                            temp = results[0, -1, :]
+                            temp = temp.cpu().numpy()
+                            result = softmax(temp)
+                            word_1 = tokenizer.encode(' Yes')[0]
+                            word_2 = tokenizer.encode(' No')[0]
+
+                        pred_id = torch.argmax(results[0, -1, :]).item()
+                        # print("\nPredicted token ID of next word: ")
+                        # print(pred_id)
+
+                        pred_word = tokenizer.decode(pred_id)
+                        # print("\nPredicted next word for sequence: ")
+                        # print(pred_word)
+
+                        response['context'] = medical_context_file.split('.')[0]
+                        response['yes_prob'] = result[word_1]
+                        response['no_prob'] = result[word_2]
+                        response['next_word_pred'] = pred_word
+                        response['closed_prompt_race'] = closed_race
+                        response['closed_prompt_gender'] = closed_gender
+                        response['open_prompt_race'] = r
+                        response['open_prompt_gender'] = g
+                        response['open_prompt_name'] = names[r][g][q]
+                        response['open_vignette_num'] = q
+                        response['yes_prompt_vignette_num'] = i
+                        out1.append(response)
 
 # print(out1)
 results_data1 = pd.DataFrame(out1)
-results_data1.to_csv("results/" + medical_context_file.split('.')[0] + "_" + opts.closed_prompt + '_results.csv')
+results_data1.to_csv("iterated_results/" + medical_context_file.split('.')[0] + "_" + opts.closed_prompt + '_results.csv')
---
+++ iterated_results/README.md
@@ -0,0 +1,1 @@
+iterated results go here
